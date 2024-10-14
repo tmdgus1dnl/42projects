cd99415beed6de5bfa91a56e9c769df233d671d2
Seung Hyun Han <136709860+tmdgus1dnl@users.noreply.github.com>
2024-10-14 16:23:19 +0900

    cpp09 ex02 is_sorted delete commit

--- 42_projects/cpp/cpp09/ex02/PmergeMe.cpp
+++ 42_projects/cpp/cpp09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: seunghan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 11:40:35 by seunghan          #+#    #+#             */
-/*   Updated: 2024/10/10 19:20:28 by seunghan         ###   ########.fr       */
+/*   Updated: 2024/10/14 15:58:10 by seunghan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,23 @@ PmergeMe::PmergeMe(const char** argv)
 }
 
 PmergeMe::~PmergeMe() {}
+
+PmergeMe::PmergeMe(const PmergeMe& other)
+{
+	*this = other;
+}
+
+PmergeMe& PmergeMe::operator=(const PmergeMe& other)
+{
+	if (this != &other)
+	{
+		this->originalData = other.getData();
+		this->vec = other.getVec();
+		this->lst = other.getLst();
+	}
+	
+	return (*this);
+}
 
 void PmergeMe::parseInput(double num, const char* ptr, const char* str) const
 {
@@ -43,7 +60,7 @@ void PmergeMe::pushNums(const char** argv)
 	}
 }
 
-void PmergeMe::pushVecs(intVec2D& vec2D) const
+void PmergeMe::pushVecs(Vec2D& vec2D) const
 {
 	for (std::size_t i = 0; i < originalData.size(); ++i)
 	{
@@ -54,23 +71,46 @@ void PmergeMe::pushVecs(intVec2D& vec2D) const
 	}
 }
 
-void PmergeMe::FJSort()
+void PmergeMe::pushLsts(Lst2D& lst2D) const
 {
-	intVec2D mainChainVec;
-
-	pushVecs(mainChainVec);
-	FJSortVec(mainChainVec);
-	getSortedVec(mainChainVec);
-
-	if (std::is_sorted(vec.begin(), vec.end()) == true)
-		std::cout << "true!" << std::endl;
-	else
-		std::cout << "false" << std::endl;
+	for (std::size_t i = 0; i < originalData.size(); ++i)
+	{
+		std::list<int> elementLst;
+		
+		elementLst.push_back(originalData[i]);
+		lst2D.push_back(elementLst);
+	}
 }
 
-PmergeMe::intVec2D PmergeMe::FJSortVec(intVec2D& mainChain) const
+void PmergeMe::FJSort()
 {
-	intVec2D rest;
+	Vec2D mainChainVec;
+	Lst2D mainChainLst;
+	clock_t start, end;
+
+	printBefore();
+
+	pushVecs(mainChainVec);
+	start = clock();
+	FJSortVec(mainChainVec);
+	getSortedVec(mainChainVec);
+	end = clock();
+
+	printAfter();
+	printTimeSpent(end - start, "vector");
+
+	pushLsts(mainChainLst);
+	start = clock();
+	FJSortLst(mainChainLst);
+	getSortedLst(mainChainLst);
+	end = clock();
+
+	printTimeSpent(end - start, "list");
+}
+
+PmergeMe::Vec2D PmergeMe::FJSortVec(Vec2D& mainChain) const
+{
+	Vec2D rest;
 
 	if (mainChain.size() > 3)
 		splitChain(mainChain, rest);
@@ -86,9 +126,27 @@ PmergeMe::intVec2D PmergeMe::FJSortVec(intVec2D& mainChain) const
 	return (mainChain);
 }
 
-void PmergeMe::splitChain(intVec2D& mainChain, intVec2D& rest) const
+PmergeMe::Lst2D PmergeMe::FJSortLst(Lst2D& mainChain) const
 {
-	intVec2D newMainChain;
+	Lst2D rest;
+
+	if (mainChain.size() > 3)
+		splitChain(mainChain, rest);
+
+	if (mainChain.size() > 3)
+		mainChain = FJSortLst(mainChain);
+	else
+		mainChain.sort();
+
+	if (rest.empty() == false)
+		binaryInsert(mainChain, rest);
+
+	return (mainChain);
+}
+
+void PmergeMe::splitChain(Vec2D& mainChain, Vec2D& rest) const
+{
+	Vec2D newMainChain;
 	std::size_t i = 0;
 	std::size_t j = 0;
 
@@ -116,7 +174,45 @@ void PmergeMe::splitChain(intVec2D& mainChain, intVec2D& rest) const
 	mainChain = newMainChain;
 }
 
-void PmergeMe::binaryInsert(intVec2D& mainChain, intVec2D& rest) const
+void PmergeMe::splitChain(Lst2D& mainChain, Lst2D& rest) const
+{
+	Lst2D newMainChain;
+	lst2D_it main_it = mainChain.begin();
+	lst2D_it newMain_it;
+	lst2D_it nextMain_it;
+	std::size_t j = 0;
+
+	while (main_it != mainChain.end() && main_it != --(mainChain.end()))
+	{
+		nextMain_it = getNextIt(main_it, 1);
+
+		if (*(main_it->begin()) <= *(nextMain_it->begin()))
+		{
+			newMainChain.push_back(*nextMain_it);
+			rest.push_back(*main_it);
+		}
+		else
+		{
+			newMainChain.push_back(*main_it);
+			rest.push_back(*nextMain_it);
+		}
+
+		if (!j)
+			newMain_it = newMainChain.begin();
+		else
+			++newMain_it;
+
+		newMain_it->push_back(j);
+		++j;
+		std::advance(main_it, 2);
+		if (main_it == --(mainChain.end()))
+			rest.push_back(*main_it);
+	}
+
+	mainChain = newMainChain;
+}
+
+void PmergeMe::binaryInsert(Vec2D& mainChain, Vec2D& rest) const
 {
 	std::vector<std::size_t> restIndex;
 	std::vector<std::size_t> Jacobs;
@@ -144,7 +240,38 @@ void PmergeMe::binaryInsert(intVec2D& mainChain, intVec2D& rest) const
 	}
 }
 
-int PmergeMe::binarySearch(intVec2D& mainChain, int restValue, int high) const
+void PmergeMe::binaryInsert(Lst2D& mainChain, Lst2D& rest) const
+{
+	std::list<std::size_t> restIndex;
+	std::list<std::size_t> Jacobs;
+
+	moveIndex(mainChain, rest, restIndex);
+
+	makeJacobsNums(Jacobs, restIndex);
+
+	int high = 1;
+	for (Tlst_it J_it = Jacobs.begin(); J_it != Jacobs.end(); ++J_it)
+	{
+
+		for (std::size_t j = *J_it - 1; J_it == Jacobs.begin() || 
+				j > *(getPrevIt(J_it, 1)) - 1; --j)
+		{
+			lst2D_it main_it = mainChain.begin();
+			std::advance(main_it, binarySearch(mainChain, 
+						*(getRest_it(rest, restIndex, j)->begin()), high - 1));
+			mainChain.insert(main_it, *(getRest_it(rest, restIndex, j)));
+
+			if (J_it == Jacobs.begin())
+				break ;
+		}
+
+		high = high * 2 + 1;
+		if (high > static_cast<int>(mainChain.size()))
+			high = static_cast<int>(mainChain.size());
+	}
+}
+
+int PmergeMe::binarySearch(Vec2D& mainChain, int restValue, int high) const
 {
 	int low = 0;
 	int mid;
@@ -168,13 +295,55 @@ int PmergeMe::binarySearch(intVec2D& mainChain, int restValue, int high) const
 		return (mid + 1);
 }
 
-void PmergeMe::moveIndex(intVec2D& mainChain, intVec2D& rest, 
+int PmergeMe::binarySearch(Lst2D& mainChain, int restValue, int high) const
+{
+	lst2D_it main_it;
+	int low = 0;
+	int mid;
+
+	mid = low + (high - low) / 2;
+	while (low < high)
+	{
+		main_it = getNextIt(mainChain.begin(), mid);
+
+		if (*(main_it->begin()) > restValue)
+			high = mid - 1;
+		else if (*(main_it->begin()) < restValue)
+			low = mid + 1;
+		else
+			break ;
+
+		mid = low + (high - low) / 2;
+	}
+	main_it = getNextIt(mainChain.begin(), mid);
+
+	if (*(main_it->begin()) >= restValue)
+		return (mid);
+	else
+		return (mid + 1);
+}
+
+void PmergeMe::moveIndex(Vec2D& mainChain, Vec2D& rest, 
 		std::vector<std::size_t>& restIndex) const
 {
 	for (std::size_t i = 0; i < mainChain.size(); ++i)
 	{
 		restIndex.push_back(mainChain[i].back());
 		mainChain[i].pop_back();
+	}
+
+	if (restIndex.size() != rest.size())
+		restIndex.push_back(rest.size() - 1); // 나머지 요소가 존재할때
+											  // 나머지 요쇼 인덱스 삽입
+}
+
+void PmergeMe::moveIndex(Lst2D& mainChain, Lst2D& rest, 
+		std::list<std::size_t>& restIndex) const
+{
+	for (lst2D_it main_it = mainChain.begin(); main_it != mainChain.end(); ++main_it)
+	{
+		restIndex.push_back(main_it->back());
+		main_it->pop_back();
 	}
 
 	if (restIndex.size() != rest.size())
@@ -190,31 +359,117 @@ void PmergeMe::makeJacobsNums(std::vector<std::size_t>& Jacobs,
 	else
 		return ;
 
-	int n = 2;
-	while (Jacobs.back() < restIndex.size())
-	{
+	for (int n = 2; Jacobs.back() < restIndex.size(); ++n)
 		Jacobs.push_back(Jacobs[n - 1] + 2 * Jacobs[n - 2]);
-		++n;
-	}
 
 	Jacobs.back() = restIndex.size();
+
 }
 
-void PmergeMe::getSortedVec(intVec2D& mainChain)
+void PmergeMe::makeJacobsNums(std::list<std::size_t>& Jacobs, 
+		std::list<std::size_t>& restIndex) const
+{
+	Jacobs.push_back(1);
+	if (restIndex.size() > 1)
+		Jacobs.push_back(3);
+	else
+		return ;
+
+	Tlst_it J_it = Jacobs.begin();
+	std::advance(J_it, 2);
+
+	while (Jacobs.back() < restIndex.size())
+		Jacobs.push_back(*(getPrevIt(J_it, 1)) + 2 * *(getPrevIt(J_it, 2)));
+
+	Jacobs.back() = restIndex.size();
+
+}
+
+PmergeMe::lst2D_it PmergeMe::getRest_it(Lst2D& rest, 
+		std::list<std::size_t> restIndex, std::size_t j) const
+{
+	Tlst_it restIndex_it = getNextIt(restIndex.begin(), j);
+
+	return (getNextIt(rest.begin(), *restIndex_it));
+}
+
+PmergeMe::lst2D_it PmergeMe::getNextIt(lst2D_it it, int n) const
+{
+	for (int i = 0; i < n; ++i)
+		++it;
+
+	return (it);
+}
+
+PmergeMe::Tlst_it PmergeMe::getNextIt(Tlst_it it, int n) const
+{
+	for (int i = 0; i < n; ++i)
+		++it;
+
+	return (it);
+}
+
+PmergeMe::Tlst_it PmergeMe::getPrevIt(Tlst_it it, int n) const
+{
+	for (int i = 0; i < n; ++i)
+		--it;
+
+	return (it);
+}
+
+void PmergeMe::getSortedVec(Vec2D& mainChain)
 {
 	for (std::size_t i = 0; i < mainChain.size(); ++i)
 		this->vec.push_back(mainChain[i][0]);
 }
 
-void PmergeMe::print2DVec(intVec2D vec) const
+void PmergeMe::getSortedLst(Lst2D& mainChain)
 {
-	for (std::size_t i = 0; i < vec.size(); ++i)
-	{
-		for (std::size_t j = 0; j < vec[i].size(); ++j)
-			std::cout << vec[i][j] << " ";
+	for (lst2D_it main_it = mainChain.begin(); main_it != mainChain.end(); ++main_it)
+		this->lst.push_back(*(main_it->begin()));
+}
 
-		std::cout << "    ";
-	}
-
+void PmergeMe::printBefore() const
+{
+	std::cout << "Before :  ";
+	for (std::size_t i = 0; i < originalData.size(); ++i)
+		std::cout << originalData[i] << " ";
 	std::cout << std::endl;
+}
+
+void PmergeMe::printAfter() const
+{
+	std::cout << "After :   ";
+	for (std::size_t i = 0; i < vec.size(); ++i)
+		std::cout << vec[i] << " ";
+	std::cout << std::endl;
+}
+
+void PmergeMe::printTimeSpent(clock_t timeSpent, std::string container) const
+{
+	std::cout << "Time to process a range of ";
+
+	if (container == "vector")
+		std::cout << std::setw(4) << vec.size() << " elements with std::vector :  ";
+	else
+		std::cout << std::setw(4) << lst.size() << " elements with std::list :    ";
+
+	double timeSpentMs = static_cast<double>(timeSpent) / CLOCKS_PER_SEC * 1000;
+
+	std::cout << timeSpentMs << " ms" << std::endl;
+}
+
+const std::vector<int>& PmergeMe::getData() const
+{
+	return (this->originalData);
+}
+
+const std::vector<int>& PmergeMe::getVec() const
+{
+	return (this->vec);
+}
+
+const std::list<int>& PmergeMe::getLst() const
+{
+	return (this->lst);
 }
--- 42_projects/cpp/cpp09/ex02/PmergeMe.hpp
+++ 42_projects/cpp/cpp09/ex02/PmergeMe.hpp
@@ -6,19 +6,22 @@
 /*   By: seunghan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 11:40:53 by seunghan          #+#    #+#             */
-/*   Updated: 2024/10/10 19:15:22 by seunghan         ###   ########.fr       */
+/*   Updated: 2024/10/13 19:10:23 by seunghan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include <vector>
 #include <list>
+#include <iterator>
 #include <algorithm>
 #include <cstdlib>
 #include <cmath>
+#include <ctime>
 #include <stdexcept>
 #include <exception>
 
@@ -31,24 +34,47 @@ class PmergeMe
 		PmergeMe();
 
 	public :
-		typedef std::vector< std::vector<int> > intVec2D;
+		typedef std::vector< std::vector<int> > Vec2D;
+		typedef std::list< std::list<int> > Lst2D;
+		typedef std::list< std::list<int> >::iterator lst2D_it;
 		typedef std::list<int>::iterator lst_it;
+		typedef std::list<std::size_t>::iterator Tlst_it;
 		PmergeMe(const char** argv);
-		/*PmergeMe(const PmergeMe& other);
-		PmergeMe& operator=(const PmergeMe& other);*/
+		PmergeMe(const PmergeMe& other);
+		PmergeMe& operator=(const PmergeMe& other);
 		~PmergeMe();
+		void parseInput(double num, const char* ptr, const char* str) const;
 		void pushNums(const char** argv);
 		void FJSort();
-		void pushVecs(intVec2D& vec2D) const;
-		void parseInput(double num, const char* ptr, const char* str) const;
-		void splitChain(intVec2D& mainChain, intVec2D& rest) const;
-		void binaryInsert(intVec2D& mainChain, intVec2D& rest) const;
-		void moveIndex(intVec2D& mainChain, intVec2D& rest, 
+		void pushVecs(Vec2D& vec2D) const;
+		void pushLsts(Lst2D& lst2D) const;
+		void splitChain(Vec2D& mainChain, Vec2D& rest) const;
+		void splitChain(Lst2D& mainChain, Lst2D& rest) const;
+		void binaryInsert(Vec2D& mainChain, Vec2D& rest) const;
+		void binaryInsert(Lst2D& mainChain, Lst2D& rest) const;
+		void moveIndex(Vec2D& mainChain, Vec2D& rest, 
 				std::vector<std::size_t>& restIndex) const;
+		void moveIndex(Lst2D& mainChain, Lst2D& rest, 
+				std::list<std::size_t>& restIndex) const;
 		void makeJacobsNums(std::vector<std::size_t>& Jacobs, 
 				std::vector<size_t>& restIndex) const;
-		void getSortedVec(intVec2D& mainChain);
-		void print2DVec(intVec2D vec) const;
-		int binarySearch(intVec2D& mainChain, int restValue, int high) const;
-		intVec2D FJSortVec(intVec2D& mainChain) const;
+		void makeJacobsNums(std::list<std::size_t>& Jacobs, 
+				std::list<size_t>& restIndex) const;
+		void getSortedVec(Vec2D& mainChain);
+		void getSortedLst(Lst2D& mainChain);
+		void printBefore() const;
+		void printAfter() const;
+		void printTimeSpent(clock_t timeSpent, std::string container) const;
+		int binarySearch(Vec2D& mainChain, int restValue, int high) const;
+		int binarySearch(Lst2D& mainChain, int restValue, int high) const;
+		Vec2D FJSortVec(Vec2D& mainChain) const;
+		Lst2D FJSortLst(Lst2D& mainChain) const;
+		lst2D_it getRest_it(Lst2D& rest, std::list<std::size_t> restIndex, 
+				std::size_t j) const;
+		Tlst_it getPrevIt(Tlst_it it, int n) const;
+		Tlst_it getNextIt(Tlst_it it, int n) const;
+		lst2D_it getNextIt(lst2D_it it, int n) const;
+		const std::vector<int>& getData() const;
+		const std::vector<int>& getVec() const;
+		const std::list<int>& getLst() const;
 };
